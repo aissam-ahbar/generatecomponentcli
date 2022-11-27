8b4d3738fe22bfc07c9ca8eccb56cec2e7e73a67
aissam-ahbar <33417377+aissam-ahbar@users.noreply.github.com>
2022-11-27 18:54:38 +0100

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,14 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [
+    /**
+     *  Included by default in all Angular apps created with the CLI ng new.
+     *  Re-exports CommonModule and ApplicationModule, making their exports
+     * and providers available to all apps
+     */
+    BrowserModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
