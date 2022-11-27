90b7163d6dfe7f91261e264dcc7446adce50c182
aissam-ahbar <33417377+aissam-ahbar@users.noreply.github.com>
2022-11-27 11:42:24 +0100

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { MycompComponent } from './mycomp/mycomp.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
